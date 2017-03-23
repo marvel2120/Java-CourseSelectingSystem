a1c033197bbe099c6ef06cc714067e9c61eaa525
HongliangPan <940700785@qq.com>
2017-03-23 13:40:26 +1100

    Update StudentMenu.java

--- StudentMenu.java
+++ StudentMenu.java
@@ -18,15 +18,15 @@ public class StudentMenu extends StudentDao {
 
 	private void mainMenu() {
 		System.out.println("===================");
-		System.out.println("1.新增学生");
-		System.out.println("2.删除学生");
-		System.out.println("3.修改学生");
-		System.out.println("4.查询学生");
-		System.out.println("5.显示全部");
-		System.out.println("6.登陆");
-		System.out.println("7.退出");
+		System.out.println("1.insert student");
+		System.out.println("2.delete student");
+		System.out.println("3.modify student");
+		System.out.println("4.find student");
+		System.out.println("5.show all students");
+		System.out.println("6.login");
+		System.out.println("7.exit");
 		System.out.println("===================");
-		System.out.println("请选择(0-7):");
+		System.out.println("please choose from (0-7):");
 		int choice = checkInt();
 		switch (choice) {
 		case 1:
@@ -48,11 +48,11 @@ public class StudentMenu extends StudentDao {
 			login();
 			break;
 		case 7:
-			System.out.println("您已退出thank you!!");
+			System.out.println("Exit successfully, thank you!!");
 			System.exit(0);
 			break;
 		default:
-			System.out.println("输入有误系统终止ֹ");
+			System.out.println("The input is invaild please try again");
 			System.exit(-1);
 			break;
 		}
@@ -63,29 +63,29 @@ public class StudentMenu extends StudentDao {
 		try {
 			choice = input.nextInt();
 		} catch (Exception e) {
-			System.out.println("必须输入整数!!");
+			System.out.println("must input integers!!");
 		}
 		return choice;
 	}
 
 	// 添加学员信息
 	private void insert() {
-		System.out.println("请输入编号：");
+		System.out.println("please input sid：");
 		int sid = input.nextInt();
-		System.out.println("请输入学号：");
+		System.out.println("please input student number：");
 		input.nextLine();
 		String sno = input.nextLine();
-		System.out.println("请输入姓名：");
+		System.out.println("please input name：");
 		String sname = input.nextLine();
-		System.out.println("请输入性别：");
+		System.out.println("please input gender：");
 		String sex = input.nextLine();
-		System.out.println("请输入出生日期：****-**-**");
+		System.out.println("please input date of birth：****-**-**");
 		String sdate = input.nextLine();
-		System.out.println("请输入电话：");
+		System.out.println("please input phone number：");
 		String stel = input.nextLine();
-		System.out.println("请输入住址：");
+		System.out.println("please input address：");
 		String sadd = input.nextLine();
-		System.out.println("请输入密码：");
+		System.out.println("please input password：");
 		String pwd = input.nextLine();
 
 		Student student = new Student(sid, sno, sname, sex, sdate, stel, sadd,
@@ -101,7 +101,7 @@ public class StudentMenu extends StudentDao {
 
 	// 按学号删除
 	private void delete() {
-		System.out.println("请输入想删除的学生编号");
+		System.out.println("please input the sid who you want to delete：");
 		int sid = input.nextInt();
 		boolean result = studentDao.delete(sid);
 		if (result == true) {
@@ -114,23 +114,23 @@ public class StudentMenu extends StudentDao {
 
 	// 按学号删除
 	private void update() {
-		System.out.println("请输入你想更新的学生编号：");
+		System.out.println("please input the sid who you want to modify：");
 		int sid = input.nextInt();
-		System.out.println("请输入更新信息：");
-		System.out.println("请输入学号：");
+		System.out.println("please input update information:");
+		System.out.println("please input sid：");
 		input.nextLine();
 		String sno = input.nextLine();
-		System.out.println("请输入姓名：");
+		System.out.println("please input name：");
 		String sname = input.nextLine();
-		System.out.println("请输入姓名：");
+		System.out.println("please input gender："); 
 		String sex = input.nextLine();
-		System.out.println("请输入出生日期：****-**-**");
+		System.out.println("please input date of birth：****-**-**");
 		String sdate = input.nextLine();
-		System.out.println("请输入电话：");
+		System.out.println("please input phone number：");
 		String stel = input.nextLine();
-		System.out.println("请输入住址：");
+		System.out.println("please input address：");
 		String sadd = input.nextLine();
-		System.out.println("请输入密码：");
+		System.out.println("please input password：");
 		String pwd = input.nextLine();
 		Student student = new Student(sid, sno, sname, sex, sdate, stel, sadd,
 				pwd);
@@ -145,14 +145,14 @@ public class StudentMenu extends StudentDao {
 	}
 
 	private void search() {
-		System.out.println("==》查询学生");
-		System.out.println("输入编号：");
+		System.out.println("==》find student");
+		System.out.println("please input sid：：");
 		int sid = input.nextInt();
 		User user = studentDao.search(sid);
 		if (null != user) {
-			System.out.println("==》查询成功" + user.toString());
+			System.out.println("==》successful" + user.toString());
 		} else {
-			System.out.println("==》查询失败");
+			System.out.println("==》failed");
 		}
 		returnMain();
 	}
@@ -163,17 +163,17 @@ public class StudentMenu extends StudentDao {
 	}
 
 	private void login() {
-		System.out.println("===》登录系统");
+		System.out.println("===》login system");
 		Tools tools = new Tools();
 		String code = tools.getCode();
-		System.out.println("验证码：" + code);
-		System.out.println("请输入登录名");
+		System.out.println("verify code：" + code);
+		System.out.println("please input account name：");
 		String name = input.next();
-		System.out.println("请输入密码");
+		System.out.println("please input password");
 		String spwd = input.next();
 
 		while (true) {
-			System.out.println("��������֤��");
+			System.out.println("verify code");
 			String scode = input.next();
 			if (scode.equals(code)) {
 				User user = studentDao.login(name, spwd);
@@ -191,10 +191,10 @@ public class StudentMenu extends StudentDao {
 	}
 
 	private void returnMain() {
-		System.out.println("按0返回");
+		System.out.println("press 0 to return");
 		int answer = input.nextInt();
 		while (answer != 0) {
-			System.out.println("输入0返回");
+			System.out.println("press 0 to return");
 			answer = input.nextInt();
 		}
 		if (answer == 0) {
